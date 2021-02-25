853c30f901addced2521c5a70b4ce2af86297df0
K0tLecik <misiu12525@wp.pl>
2021-02-25 10:22:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from math import *
+
+a = 10
+b = 5
+c = 2
+
+dzielenie = a/c
+dzielenie_calkowite = b//c
+reszta = b%c
+
+potega = a**c
+
+print(dzielenie)
+print(dzielenie_calkowite)
+print(reszta)
+print(potega)
+
+a+= 2
+
+print(sqrt(25))
